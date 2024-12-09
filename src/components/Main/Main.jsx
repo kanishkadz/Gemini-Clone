3c7e23126c3b4101c2859f4147f3302daa46eec0
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-09 19:12:48 +0530

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Main.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
 
 const Main = () => {
+
+  const{onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context);
+
+
+
+
+
   return (
     <div className='main'>
         <div className="nav">
