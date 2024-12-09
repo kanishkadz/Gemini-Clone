bac7403c531a45ceb1bd693033000012fa0495c5
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-09 21:50:46 +0530

    Update SideBar.jsx

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,17 +1,16 @@
-import React, { useContext, useState } from 'react'
-import './SideBar.css'
-import { assets } from '../../assets/assets'
-import { Context } from '../../context/Context'
+import React, { useContext, useState } from 'react';
+import './SideBar.css';
+import { assets } from '../../assets/assets';
+import { Context } from '../../context/Context';
 
 const SideBar = () => {
-
     const [extended, setExtended] = useState(false);
-    const { onSent, prevPrompts, setRecent, newChat } = useContext(Context);
+    const { onSent, prevPrompts, setRecentPrompt, newChat } = useContext(Context);
 
-    const loadPrompt = async(prompt) => {
-        setRecentPrompt(prompt)
-        await onSent(prompt)
-    }
+    const loadPrompt = async (prompt) => {
+        setRecentPrompt(prompt);
+        await onSent(prompt);
+    };
 
     return (
         <div className='sidebar'>
@@ -21,22 +20,19 @@ const SideBar = () => {
                     <img src={assets.plus_icon} alt="" />
                     {extended ? <p> New Chat </p> : null}
                 </div>
-                {extended ?
+                {extended ? (
                     <div className="recent">
                         <p className='recent-title'> Recent </p>
                         {prevPrompts.map((item, index) => {
                             return (
-                                <div onClick={()=>loadPrompt(item)} className="recent-entry">
+                                <div onClick={() => loadPrompt(item)} className="recent-entry" key={index}>
                                     <img src={assets.message_icon} alt="" />
-                                    <p>{item.slice(0,18)} ...</p>
+                                    <p>{item.slice(0, 18)} ...</p>
                                 </div>
-                            )
+                            );
                         })}
-
                     </div>
-                    : null
-                }
-
+                ) : null}
             </div>
             <div className="bottom">
                 <div className="bottom-item recent-entry">
@@ -53,7 +49,7 @@ const SideBar = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default SideBar
+export default SideBar;
