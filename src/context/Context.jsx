bd816947b0676b6ed5a360cf280f0881ead31b11
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-09 18:48:23 +0530

    Update Context.jsx

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,9 +1,16 @@
 import { createContext } from "react";
+import run from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
 
+
+    const onSent = async(prompt) => {
+        await run(prompt)
+    }
+
+    onSent("what is react js")
 
 
     const contextValue = {
