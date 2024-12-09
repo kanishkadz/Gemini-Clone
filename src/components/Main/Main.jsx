c55308b2e0bac72816f304f92bbe6a18fc39dbfa
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-09 21:42:44 +0530

    debug1

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,73 +1,79 @@
-import React, { useContext } from 'react'
-import './Main.css'
-import { assets } from '../../assets/assets'
-import { Context } from '../../context/Context'
+import React, { useContext } from 'react';
+import './Main.css';
+import { assets } from '../../assets/assets';
+import { Context } from '../../context/Context';
 
 const Main = () => {
-
   const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context);
 
   return (
-    <div className='main'>
+    <div className="main">
       <div className="nav">
         <p>Gemini</p>
-        <img src={assets.user_icon} alt="" />
+        <img src={assets.user_icon} alt="User Icon" />
       </div>
       <div className="main-container">
-
-        {!showResult ?
+        {!showResult ? (
           <>
             <div className="greet">
-              <p><span>Hello, Kanishka.</span></p>
+              <p>
+                <span>Hello, Kanishka.</span>
+              </p>
               <p>How can I help you today?</p>
             </div>
             <div className="cards">
               <div className="card">
                 <p>Suggest beautiful places to see on an upcoming road trip</p>
-                <img src={assets.compass_icon} alt="" />
+                <img src={assets.compass_icon} alt="Compass Icon" />
               </div>
               <div className="card">
                 <p>Briefly summarize this concept: urban planning</p>
-                <img src={assets.bulb_icon} alt="" />
+                <img src={assets.bulb_icon} alt="Bulb Icon" />
               </div>
               <div className="card">
                 <p>Brainstorm team bonding activities for our work retreat</p>
-                <img src={assets.message_icon} alt="" />
+                <img src={assets.message_icon} alt="Message Icon" />
               </div>
               <div className="card">
                 <p>Improve the reliability of the following code</p>
-                <img src={assets.code_icon} alt="" />
+                <img src={assets.code_icon} alt="Code Icon" />
               </div>
             </div>
           </>
-          :
-          <div className='result'>
+        ) : (
+          <div className="result">
             <div className="result-title">
-              <img src={assets.user_icon} alt="" />
+              <img src={assets.user_icon} alt="User Icon" />
               <p>{recentPrompt}</p>
             </div>
             <div className="result-data">
-              <img src={assets.gemini_icon} alt="" />
-              {loading ?
-                <div className='loader'>
+              <img src={assets.gemini_icon} alt="Gemini Icon" />
+              {loading ? (
+                <div className="loader">
                   <hr />
                   <hr />
                   <hr />
                 </div>
-                : <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
-              }
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData || 'No data available.' }}></p>
+              )}
             </div>
           </div>
-        }
-
-
+        )}
         <div className="main-bottom">
           <div className="search-box">
-            <input onChange={(e) => { setInput(e.target.value) }} value={input} type="text" placeholder='Enter a prompt here' />
+            <input
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a prompt here"
+            />
             <div>
-              <img src={assets.gallery_icon} alt="" />
-              <img src={assets.mic_icon} alt="" />
-              {input?<img onClick={() => onSent()} src={assets.send_icon} alt="" />:null}
+              <img src={assets.gallery_icon} alt="Gallery Icon" />
+              <img src={assets.mic_icon} alt="Mic Icon" />
+              {input && (
+                <img onClick={() => onSent()} src={assets.send_icon} alt="Send Icon" />
+              )}
             </div>
           </div>
           <p className="bottom-info">
@@ -76,7 +82,7 @@ const Main = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
