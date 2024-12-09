a0ac7ac8008b808af446b2d856ea460eff7354a9
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-09 12:41:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import SideBar from './components/SideBar/SideBar'
+import Main from './components/Main/Main'
 
 const App = () => {
   return (
     <>
       <SideBar />
+      <Main />
     </>
   )
 }
