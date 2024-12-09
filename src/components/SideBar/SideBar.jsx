0edfb869e0750bb3eee4b76a5aaf52880067672c
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-09 12:08:40 +0530

    Update SideBar.jsx

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './SideBar.css'
+import { assets } from '../../assets/assets'
 
 const SideBar = () => {
   return (
-    <div className='sidebar'>SideBar</div>
+    <div className='sidebar'>
+        <div className="top">
+            <img src={assets.menu_icon} alt="" />
+        </div>
+        <div className="bottom">
+
+        </div>
+    </div>
   )
 }
 
