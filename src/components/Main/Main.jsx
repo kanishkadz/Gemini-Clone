c8929ffc9a71707c060c76e56009c874409b2529
Kanishka Anand <kanishkaadz@gmail.com>
2024-12-09 19:34:04 +0530

    commit13

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -48,10 +48,13 @@ const Main = () => {
             </div>
             <div className="result-data">
               <img src={assets.gemini_icon} alt="" />
-              {loading?
-              <div className='loader'> 
-              </div>
-              : <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+              {loading ?
+                <div className='loader'>
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+                : <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
               }
             </div>
           </div>
